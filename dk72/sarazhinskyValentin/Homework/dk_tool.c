9ffaaaf77bd3a5208dbca8ad1962651d1164560d
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-02-14 23:47:49 +0200

    Add files via upload

---
+++ dk72/sarazhinskyValentin/Homework/dk_tool.c
@@ -0,0 +1,8 @@
+#include "dk_tool.h"
+
+int transposition(char matrix)
+{
+
+	return 0;
+}
+
---
+++ dk72/sarazhinskyValentin/Homework/dk_tool.h
@@ -0,0 +1,11 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+int transposition(char matrix);
+
+
+#endif
+
+
